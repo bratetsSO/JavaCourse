dac5fbd049a1bf4f95e5208c8ac2e23e10b107b8
Sarokin Siarhei <c.copokuh@gmail.com>
2026-04-26 23:45:01 +0300

    JavaCourse.Lesson_01.First_Commit

---
+++ src/Course/Lesson_01/Main.java
@@ -0,0 +1,7 @@
+package Course.Lesson_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Developer => Sorokin Sergey; DataTime get task => 22/04/2026 19:46:42");
+    }
+}
